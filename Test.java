7d7c5623135cb0caac5fe8c2ef24ee3964df373a
vinod <vinod.goud86@gmail.com>
2017-12-10 15:04:28 +0530

    changed

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
-package com.test.controller;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {java
 		System.out.println("Hello World");
                 System.out.println("Wel Come");
 
